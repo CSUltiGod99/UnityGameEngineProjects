c84c9ddc7d4f73ad92c1fdb94af829dabd3b1adb
CSUltiGod99 <80710483+CSUltiGod99@users.noreply.github.com>
2023-02-26 16:53:17 +0000

    Create Unity GameObjectLocalScale.cs

---
+++ Unity GameObjectLocalScale.cs
@@ -0,0 +1,25 @@
+// CSharp - Object Transform Local Scaling:
+
+using System
+using System.Collections;
+using UnityEngine
+
+public class Example : MonoBehaviour {
+
+    public Rigidbody sRB;
+    public GameObject player;
+    public Vector3 theScale;
+    
+    void Start()
+    {
+       theScale = new Vector3<newScale>();
+    }
+    void Update()
+    {
+       TheScale
+    }
+
+
+
+
+
