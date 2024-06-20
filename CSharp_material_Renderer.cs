dd1310ba8864ddf25de72778ac47e72e5dfc1f2e
Blane <80710483+CSUltiGod99@users.noreply.github.com>
2024-06-20 15:09:05 +0100

    Update CSharp_material_Renderer.cs

--- CSharp_material_Renderer.cs
+++ CSharp_material_Renderer.cs
@@ -3,11 +3,22 @@ CSharp - Unity Change Material Renderer:
 // Source code:
 
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class 
+public class _material_Renderer : MonoBehaviour {
+
+    g
+    // james
+    g
+
+    h
+    f      
 
 
 
 
+
+
+}
 
